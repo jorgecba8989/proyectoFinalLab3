cce2c688fc712b24aa4f6b9ba01960a2c8a16d3b
jorgecba8989 <georgecba89@gmail.com>
2022-07-28 17:45:24 -0300

    agregado de busqueda de comic de un personaje marvel

--- src/app/personajes/personajes.component.ts
+++ src/app/personajes/personajes.component.ts
@@ -8,37 +8,63 @@ import { ApiMarvelService } from '../services/api-marvel.service';
 })
 export class PersonajesComponent implements OnInit {
 
-  allCharacters:any=[] = new Array<any>();
-
-  characterName:string = "";
-  showSearchResult:boolean = false;
+  allCharacters:any=[];
+  comics:any=[];
+  series:any=[];
+  showComicsDiv!:boolean;
+  showSeriesDiv!:boolean;
+  characterName!:string;
+  showSearchResult!:boolean;
   searchedCharacter:any=[];
 
   constructor(private service:ApiMarvelService) { }
 
+
+
   ngOnInit(): void {
-  
+
+    this.showComicsDiv = false;
+    this.showSeriesDiv = false;
     this.showSearchResult = false;
     this.service.allCharacters().subscribe((result)=>{
+      console.log(result);
       this.allCharacters = result.data.results;
     })
+
   }
 
-
-  findCharacter(item:any)
+  findCharacter(event:any)
 {
-   this.characterName = item.target.value;
+   this.characterName = event.target.value;
+   console.log(this.characterName);
    this.service.getCharacterByName(this.characterName).subscribe((result)=>{
+     console.log(result);
      if(result.data.count>0)
      {
        this.showSearchResult = true;
        this.searchedCharacter = result.data.results;
      }
      else{
+
        this.ngOnInit();
      }
    })
-
 }
+
+  fetchComicsByCharacter(characterId:string)
+  {
+    this.service.getComicsByCharacter(characterId).subscribe((result)=>{
+
+      if(result.data.count>0)
+      {
+        this.comics = result.data.results;
+        this.showComicsDiv = true;
+      }
+    })
+  }
+
+
+
+
 
 }
