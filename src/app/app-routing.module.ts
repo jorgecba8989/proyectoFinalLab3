b918db9d620d0643b759505ab8572ac31b574f59
jorgecba8989 <georgecba89@gmail.com>
2022-07-27 23:01:04 -0300

    agregado de ruta personajes marvel

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,16 +3,19 @@ import { RouterModule, Routes } from '@angular/router';
 import { ComicDescripcionComponent } from './comic-descripcion/comic-descripcion.component';
 import { ComicsComponent } from './comics/comics.component';
 import { MisFavoritosComponent } from './mis-favoritos/mis-favoritos.component';
+import { PersonajesComponent } from './personajes/personajes.component';
 import { SeriesComponent } from './series/series.component';
 
 
 const routes: Routes = [
-
+  
+  {path: '' , redirectTo:'favoritos' , pathMatch:'full'},
   {path:'favoritos',component:MisFavoritosComponent},
   {path:'comics',component:ComicsComponent},
   {path:'series',component:SeriesComponent},
   {path: 'ver/:id', component:ComicDescripcionComponent},
-  {path: 'ver/:favorito/:id', component:ComicDescripcionComponent}
+  {path: 'ver/:favorito/:id', component:ComicDescripcionComponent},
+  {path: 'personajes',component:PersonajesComponent}
 
 ];
 
